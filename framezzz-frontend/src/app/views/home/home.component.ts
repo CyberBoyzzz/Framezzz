d135e5848174ec9f1bb969bcc3afc1a524646e95
gzaprzala <zaprzala.grzegorz@gmail.com>
2025-01-11 10:01:09 +0100

    feat: added basic app toolbar

--- framezzz-frontend/src/app/views/home/home.component.ts
+++ framezzz-frontend/src/app/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ComicsCardComponent } from '../../components/comics-card/comics-card.component';
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ComicsCardComponent],
+  imports: [ComicsCardComponent, ToolbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
